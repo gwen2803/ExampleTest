90dc2f42801180c9b6a252f488ac738d82d58713
gwen2803 <gwen2803@gmail.com>
2025-09-07 15:24:02 +0900

    project init

---
+++ ExampleForTest/ExampleForTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleForTestApp.swift
+//  ExampleForTest
+//
+//  Created by YONGDOLMAC on 9/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ExampleForTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExampleForTestTests/ExampleForTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleForTestTests.swift
+//  ExampleForTestTests
+//
+//  Created by YONGDOLMAC on 9/7/25.
+//
+
+import Testing
+@testable import ExampleForTest
+
+struct ExampleForTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
